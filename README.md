b7cd9f03ed74747b0970e57196ab49c150893fab
Sripriya Ranganathan <priya_emmkay@yahoo.com>
2023-12-19 12:15:37 +0530

    check

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sample
+# AIM 
+
+# Theory
---
+++ fact.py
@@ -0,0 +1,10 @@
+import os
+def fact(n):
+    '''
+     write your code
+'''
+
+print("Enter a number:")
+n=int(input())
+r=fact(n)
+print("factorial:",n)
